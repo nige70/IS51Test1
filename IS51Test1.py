e37a2c7b84d781538eb224af9614648b3e22dd73
Nigel <nigel.vigan@gmail.com>
2020-11-12 17:03:25 -0800

    first commit

--- IS51Test1.py
+++ IS51Test1.py
@@ -1,20 +1,4 @@
 
-"""
-The program is trying to determine if Option1 or Option2 pays more money.
-Option1 pays 100 dollars per day for 10 days. Option2 pays 1 dollar the first
-day and eventually doubles each day for 10 days. Two functions will be used to
-calculate the pay rate.
-Function1 will calculate the amount for Option1, and Function2 will calculate
-the amount for Option2.
-
-Function1 will output 100 dollars * 10 days.
-Function2 will loop 10 times, with each time doubling the amount and adding it to the total.
-
-If the amount is equal, we will output to the user "Option 1 and Option 2 pays the same. "
-If Option1 is better, we will output to the user "Option 1 is better. "
-If Option2 is better, we will output to the user "Option 2 is better. "
-"""
-
 
 
 
